baa1fce78710762f79f3dab3d2302edb99795ce8
Fernanda Sudré <fernanda.sudre@gmail.com>
2020-12-04 00:44:20 -0300

    Mudanças no layout das paginas

---
+++ src/main/java/br/org/garota_progr_ame/controller/SiteController.java
@@ -0,0 +1,13 @@
+package br.org.garota_progr_ame.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SiteController {
+    	
+	@GetMapping("/site")
+	public String init() {
+		return "Site/index";		
+	}
+}
